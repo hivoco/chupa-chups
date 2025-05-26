15071baebfdd5e2d89fc90d286e723c5d0a167b2
Krishna-hivoco <krishna@hivoco.com>
2025-05-26 13:14:02 +0530

    Adding favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -38,11 +38,30 @@ export default function App({ Component, pageProps }) {
 
         <meta property="og:image" content="/images/Chupa-Chups.png" />
 
-        <title>Legends Face Off</title>
+        <title>Forever Fun with Chupa Chups</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main className={inter.className}>
