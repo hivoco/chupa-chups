738456b7e0ceaac9eafd404de17f55b945606a4e
Krishna-hivoco <krishna@hivoco.com>
2025-05-27 12:14:36 +0530

    change text

--- src/pages/result.js
+++ src/pages/result.js
@@ -9,7 +9,7 @@ const Result = () => {
   const rewardMessages = {
     correct: {
       title: "Great!",
-      message: "You get a chance to win Chupa Chups goodies.",
+      message: "You get a chance to win goodies.",
     },
     incorrect: {
       title: "Oops!",
